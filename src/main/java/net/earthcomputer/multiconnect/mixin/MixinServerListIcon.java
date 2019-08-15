bf0f538b1999eb7328df4c5ff295434bfb7a4bb2
Joe <burtonjae@hotmail.co.uk>
2019-08-15 19:38:11 +0100

    Show that you can connect to supported servers in the server list

---
+++ src/main/java/net/earthcomputer/multiconnect/mixin/MixinServerListIcon.java
@@ -0,0 +1,26 @@
+package net.earthcomputer.multiconnect.mixin;
+
+import com.mojang.bridge.game.GameVersion;
+import net.earthcomputer.multiconnect.protocol.Protocols;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
+import net.minecraft.client.options.ServerEntry;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(MultiplayerServerListWidget.ServerItem.class)
+public class MixinServerListIcon {
+
+    @Shadow @Final private ServerEntry server;
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/bridge/game/GameVersion;getProtocolVersion()I", remap = false))
+    public int redirectProtocolVersion(GameVersion version, int oldProtocolVersion) {
+        if (!Protocols.isSupported(server.protocolVersion))
+            return oldProtocolVersion;
+        else
+            return server.protocolVersion;
+    }
+
+}
