98cd8c8e9efb2ea39eebd5e9dfe015620a63ca58
Joe <burtonjae@hotmail.co.uk>
2019-08-16 21:33:25 +0100

    1.14.1 support

---
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_14_1/Protocol_1_14_1.java
@@ -0,0 +1,6 @@
+package net.earthcomputer.multiconnect.protocols.v1_14_1;
+
+import net.earthcomputer.multiconnect.protocols.v1_14_2.Protocol_1_14_2;
+
+public class Protocol_1_14_1 extends Protocol_1_14_2 {
+}
---
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_14_1/mixin/MixinClientPlayerEntity.java
@@ -0,0 +1,30 @@
+package net.earthcomputer.multiconnect.protocols.v1_14_1.mixin;
+
+import net.earthcomputer.multiconnect.ConnectionInfo;
+import net.earthcomputer.multiconnect.protocols.Protocols;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientPlayerEntity.class)
+public class MixinClientPlayerEntity {
+
+    @Redirect(method = "isInSneakingPose", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isSwimming()Z"))
+    public boolean redirectIsSneakingWhileSwimming(ClientPlayerEntity _this) {
+        if (ConnectionInfo.protocolVersion <= Protocols.V1_14_1)
+            return false;
+        else
+            return _this.isSwimming();
+    }
+
+    @Inject(method = "method_20623", at = @At("HEAD"), cancellable = true)
+    public void easierUnderwaterSprinting(CallbackInfoReturnable<Boolean> ci) {
+        if (ConnectionInfo.protocolVersion <= Protocols.V1_14_1) {
+            ci.setReturnValue(((ClientPlayerEntity) (Object) this).input.movementForward >= 0.8);
+        }
+    }
+
+}
