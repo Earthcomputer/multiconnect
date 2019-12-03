b11e5a1dce745829c41838fd4f26baecd30a27d2
Joe <burtonjae@hotmail.co.uk>
2019-12-03 23:54:47 +0000

    Fix sneaking

---
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/mixin/MixinOtherClientPlayerEntity.java
@@ -0,0 +1,28 @@
+package net.earthcomputer.multiconnect.protocols.v1_13_2.mixin;
+
+import com.mojang.authlib.GameProfile;
+import net.earthcomputer.multiconnect.api.Protocols;
+import net.earthcomputer.multiconnect.impl.ConnectionInfo;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.OtherClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OtherClientPlayerEntity.class)
+public abstract class MixinOtherClientPlayerEntity extends AbstractClientPlayerEntity {
+
+    public MixinOtherClientPlayerEntity(ClientWorld world, GameProfile gameProfile) {
+        super(world, gameProfile);
+    }
+
+    @Inject(method = "updateSize", at = @At("HEAD"))
+    private void onUpdateSize(CallbackInfo ci) {
+        if (ConnectionInfo.protocolVersion <= Protocols.V1_13_2) {
+            super.updateSize();
+        }
+    }
+
+}
---
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/mixin/MixinPlayerEntity.java
@@ -0,0 +1,37 @@
+package net.earthcomputer.multiconnect.protocols.v1_13_2.mixin;
+
+import net.earthcomputer.multiconnect.api.Protocols;
+import net.earthcomputer.multiconnect.impl.ConnectionInfo;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public class MixinPlayerEntity {
+
+    private static final EntityDimensions SNEAKING_DIMENSIONS_1_13_2 = EntityDimensions.changing(0.6f, 1.65f);
+
+    @Inject(method = "getDimensions", at = @At("HEAD"), cancellable = true)
+    private void onGetDimensions(EntityPose pose, CallbackInfoReturnable<EntityDimensions> ci) {
+        if (ConnectionInfo.protocolVersion <= Protocols.V1_13_2) {
+            if (pose == EntityPose.SNEAKING) {
+                ci.setReturnValue(SNEAKING_DIMENSIONS_1_13_2);
+            }
+        }
+    }
+
+    @ModifyConstant(method = "getActiveEyeHeight", constant = @Constant(floatValue = 1.27f))
+    private float modifySneakEyeHeight(float prevEyeHeight) {
+        if (ConnectionInfo.protocolVersion > Protocols.V1_13_2)
+            return prevEyeHeight;
+        else
+            return 1.54f;
+    }
+
+}
