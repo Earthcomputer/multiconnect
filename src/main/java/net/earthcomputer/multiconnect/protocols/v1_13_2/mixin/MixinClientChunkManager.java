f69f19193b288eb19262befcadf5df7891ba4a79
Joe <burtonjae@hotmail.co.uk>
2019-12-04 01:16:24 +0000

    Fix heightmaps

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/mixin/MixinClientChunkManager.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/mixin/MixinClientChunkManager.java
@@ -5,14 +5,19 @@ import net.earthcomputer.multiconnect.impl.ConnectionInfo;
 import net.earthcomputer.multiconnect.protocols.v1_13_2.Protocol_1_13_2;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientChunkManager;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.PacketByteBuf;
 import net.minecraft.world.Heightmap;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Arrays;
 import java.util.Set;
@@ -22,20 +27,29 @@ import java.util.stream.Collectors;
 @Mixin(ClientChunkManager.class)
 public class MixinClientChunkManager {
 
-    private static final Set<Heightmap.Type> CLIENT_HEIGHTMAPS = Arrays.stream(Heightmap.Type.values()).filter(Heightmap.Type::shouldSendToClient).collect(Collectors.toSet());
+    @Unique private static final Set<Heightmap.Type> CLIENT_HEIGHTMAPS = Arrays.stream(Heightmap.Type.values()).filter(Heightmap.Type::shouldSendToClient).collect(Collectors.toSet());
+
+    @Unique private ThreadLocal<WorldChunk> chunk = new ThreadLocal<>();
 
     // ModifyVariable just to capture the chunk
     @ModifyVariable(method = "loadChunkFromPacket", ordinal = 0, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/WorldChunk;getSectionArray()[Lnet/minecraft/world/chunk/ChunkSection;"))
-    private WorldChunk recalculateClientHeightmaps(WorldChunk chunk) {
+    private WorldChunk grabChunk(WorldChunk chunk) {
+        this.chunk.set(chunk);
+        return chunk;
+    }
+
+    @Inject(method = "loadChunkFromPacket", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/WorldChunk;getSectionArray()[Lnet/minecraft/world/chunk/ChunkSection;"))
+    private void recalculateHeightmaps(World world, int x, int z, PacketByteBuf buf, CompoundTag tag, int verticalStripMask, boolean fullChunk, CallbackInfoReturnable<WorldChunk> ci) {
         if (ConnectionInfo.protocolVersion <= Protocols.V1_13_2) {
-            Heightmap.populateHeightmaps(chunk, CLIENT_HEIGHTMAPS);
+            WorldChunk chunk = this.chunk.get();
             for (ChunkSection section : chunk.getSectionArray()) {
                 if (section != null) {
                     section.calculateCounts();
                 }
             }
+            Heightmap.populateHeightmaps(chunk, CLIENT_HEIGHTMAPS);
         }
-        return chunk;
+        this.chunk.set(null);
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
