ae7268f9cc02892fd4f6ab384284d02b924f8341
Joe <burtonjae@hotmail.co.uk>
2020-01-07 22:36:12 +0000

    Fix wrong pose for other players

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/mixin/MixinOtherClientPlayerEntity.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/mixin/MixinOtherClientPlayerEntity.java
@@ -6,6 +6,7 @@ import net.earthcomputer.multiconnect.impl.ConnectionInfo;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.EntityPose;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,7 +22,21 @@ public abstract class MixinOtherClientPlayerEntity extends AbstractClientPlayerE
     @Inject(method = "updateSize", at = @At("HEAD"))
     private void onUpdateSize(CallbackInfo ci) {
         if (ConnectionInfo.protocolVersion <= Protocols.V1_13_2) {
-            super.updateSize();
+            EntityPose pose;
+            if (isFallFlying()) {
+                pose = EntityPose.FALL_FLYING;
+            } else if (isSleeping()) {
+                pose = EntityPose.SLEEPING;
+            } else if (isSwimming()) {
+                pose = EntityPose.SWIMMING;
+            } else if (isUsingRiptide()) {
+                pose = EntityPose.SPIN_ATTACK;
+            } else if (isSneaking() && !abilities.flying) {
+                pose = EntityPose.CROUCHING;
+            } else {
+                pose = EntityPose.STANDING;
+            }
+            setPose(pose);
         }
     }
 
