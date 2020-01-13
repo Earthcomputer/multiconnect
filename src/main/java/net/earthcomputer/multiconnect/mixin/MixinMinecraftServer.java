88507aa86998fde31d7525249cdecbe8d0242dec
Joe <burtonjae@hotmail.co.uk>
2020-01-13 00:27:57 +0000

    Fix datapacks not counting as a resource reload

---
+++ src/main/java/net/earthcomputer/multiconnect/mixin/MixinMinecraftServer.java
@@ -0,0 +1,19 @@
+package net.earthcomputer.multiconnect.mixin;
+
+import net.earthcomputer.multiconnect.impl.ConnectionInfo;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.LevelProperties;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class MixinMinecraftServer {
+
+    @Inject(method = "reloadDataPacks", at = @At("RETURN"))
+    private void onReloadDataPacks(LevelProperties levelProperties, CallbackInfo ci) {
+        ConnectionInfo.stopReloadingResources();
+    }
+
+}
