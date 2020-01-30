4f9a9b38aab5b1324d577c685a07edf02d3b6a27
TIVJ-dev <tivj-dev@outlook.com>
2020-01-30 21:32:21 +0200

    Revert "Swim speed fixes"
    This reverts commit 055937d22b32ac686cdb3b7b841e01a26cda0a85.

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_12_2/mixin/MixinEntity.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_12_2/mixin/MixinEntity.java
@@ -5,7 +5,6 @@ import net.earthcomputer.multiconnect.impl.ConnectionInfo;
 import net.minecraft.entity.Entity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
-import net.minecraft.tag.FluidTags;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
@@ -30,14 +29,6 @@ public abstract class MixinEntity {
     @Shadow public abstract void setVelocity(Vec3d velocity);
 
     @Shadow public abstract Vec3d getVelocity();
-
-    @Shadow public abstract double getWaterHeight();
-
-    @Shadow public abstract boolean isTouchingWater();
-
-    @Shadow public abstract boolean isSubmergedInWater();
-
-    @Shadow public abstract boolean isSubmergedIn(Tag<Fluid> fluidTag, boolean requireLoadedChunk);
 
     @Inject(method = "setSwimming", at = @At("HEAD"), cancellable = true)
     private void onSetSwimming(boolean swimming, CallbackInfo ci) {
@@ -92,13 +83,6 @@ public abstract class MixinEntity {
 
         this.waterHeight = waterHeight;
         ci.setReturnValue(foundFluid);
-    }
-
-    @Inject(method = "isTouchingWater", at = @At(value = "RETURN"), cancellable = true)
-    private void correctStatus(CallbackInfoReturnable<Boolean> cir) {
-        if (cir.getReturnValueZ() && !isSubmergedIn(FluidTags.WATER, true) && getWaterHeight() < 0.28888931871D) {
-            cir.setReturnValue(false);
-        }
     }
 
 }
