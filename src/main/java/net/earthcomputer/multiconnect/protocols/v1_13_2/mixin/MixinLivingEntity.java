e89ce31369b2be5c605e913528dc71305e5082a0
Joe <burtonjae@hotmail.co.uk>
2020-05-13 14:33:06 +0100

    Fix #29 - Walking/sprinting speeds are different between protocols.

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/mixin/MixinLivingEntity.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/mixin/MixinLivingEntity.java
@@ -5,14 +5,10 @@ import net.earthcomputer.multiconnect.impl.ConnectionInfo;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity {
@@ -21,14 +17,6 @@ public abstract class MixinLivingEntity extends Entity {
 
     public MixinLivingEntity(EntityType<?> type, World world) {
         super(type, world);
-    }
-
-    @ModifyConstant(method = "getMovementSpeed(F)F", constant = @Constant(floatValue = 0.21600002f))
-    private float modifyMovementFactor(float oldFactor) {
-        if (ConnectionInfo.protocolVersion <= Protocols.V1_13_2) {
-            return 0.546f * 0.546f * 0.546f;
-        }
-        return oldFactor;
     }
 
     @Redirect(method = "method_26318", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;jumping:Z"))
@@ -49,30 +37,12 @@ public abstract class MixinLivingEntity extends Entity {
         return horizontalCollision;
     }
 
-    @Unique private double travel_d;
-
-    @Inject(method = "travel",
-            slice = @Slice(from = @At(value = "INVOKE", target = "Ljava/lang/Math;abs(D)D", ordinal = 0, remap = false)),
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setVelocity(DDD)V", ordinal = 0),
-            locals = LocalCapture.CAPTURE_FAILHARD)
-    private void capture_d(Vec3d travelVec, CallbackInfo ci, double d) {
-        travel_d = d;
-    }
-
-    @ModifyArg(method = "travel",
-            index = 1,
-            slice = @Slice(from = @At(value = "INVOKE", target = "Ljava/lang/Math;abs(D)D", ordinal = 0, remap = false)),
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setVelocity(DDD)V", ordinal = 0))
-    private double modifyYVelocity(double yVelocity) {
+    @ModifyVariable(method = "method_26317(DZLnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", ordinal = 0, at = @At("HEAD"))
+    private boolean modifyMovingDown(boolean movingDown) {
         if (ConnectionInfo.protocolVersion <= Protocols.V1_13_2) {
-            double yVel = getVelocity().y;
-            if (yVel <= 0 && Math.abs(yVel - 0.005) >= 0.003 && Math.abs(yVel - travel_d / 16) < 0.003) {
-                return -0.003;
-            } else {
-                return yVel - travel_d / 16;
-            }
+            return true;
         }
-        return yVelocity;
+        return movingDown;
     }
 
 }
