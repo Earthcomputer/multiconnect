60994bd1ab1c51a02aa9c0214da098aa58105e03
Joe <burtonjae@hotmail.co.uk>
2020-05-15 15:07:23 +0100

    Fix sneaking not slowing you down

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_14_1/mixin/MixinClientPlayerEntity.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_14_1/mixin/MixinClientPlayerEntity.java
@@ -7,12 +7,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
 public class MixinClientPlayerEntity {
 
-    @Redirect(method = "isInSneakingPose", at = @At(value = "FIELD", target = "Lnet/minecraft/client/network/ClientPlayerEntity;field_23093:Z"))
+    @Redirect(method = "tickMovement",
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isWalking()Z")),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isSwimming()Z", ordinal = 0))
     public boolean redirectIsSneakingWhileSwimming(ClientPlayerEntity _this) {
         if (ConnectionInfo.protocolVersion <= Protocols.V1_14_1)
             return false;
