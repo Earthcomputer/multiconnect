50462ce3ade9eab448e22ff28737dadd4ce654dc
Joe <burtonjae@hotmail.co.uk>
2020-06-03 16:45:56 +0100

    Remove unused import

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/Protocol_1_13_2.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_13_2/Protocol_1_13_2.java
@@ -60,7 +60,6 @@ import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
-import net.minecraft.world.chunk.PalettedContainer;
 
 import java.util.List;
 import java.util.OptionalInt;
