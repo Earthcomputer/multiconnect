4e877c28fba8f9c1b640a6e9ea947e29b17a6163
Joe <burtonjae@hotmail.co.uk>
2020-07-31 19:51:58 +0100

    Intellij is now smarter...

--- src/main/java/net/earthcomputer/multiconnect/impl/MixinHelper.java
+++ src/main/java/net/earthcomputer/multiconnect/impl/MixinHelper.java
@@ -3,12 +3,17 @@ package net.earthcomputer.multiconnect.impl;
 public class MixinHelper {
 
     /**
-     * Pretends to create an instance of an object inside a static accessor.
-     * Do this instead of returning null so the IDE doesn't complain about nullability
+     * Pretends to be unpredictable about what can be returned inside a static accessor.
+     * Do this instead of returning null or throwing an exception so the IDE doesn't try to be smart and give warnings.
      */
     @SuppressWarnings("unchecked")
     public static <T> T fakeInstance() {
-        return (T) new Object();
+        // intellij is getting smarter, but this fools it for now...
+        if (Math.random() < 0.5) {
+            return (T) new Object();
+        } else {
+            return null;
+        }
     }
 
 }
