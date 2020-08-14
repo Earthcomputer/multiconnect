d4085c0cf39a7d0339ead11792c45a777ec9f119
Joe <burtonjae@hotmail.co.uk>
2020-08-14 11:42:01 +0100

    Only copy recipe book unlocking settings if the server is on 1.16.1 or below

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_16_1/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_16_1/mixin/MixinClientPlayNetworkHandler.java
@@ -1,5 +1,7 @@
 package net.earthcomputer.multiconnect.protocols.v1_16_1.mixin;
 
+import net.earthcomputer.multiconnect.api.Protocols;
+import net.earthcomputer.multiconnect.impl.ConnectionInfo;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -16,16 +18,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinClientPlayNetworkHandler {
     @Inject(method = "onUnlockRecipes", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/util/thread/ThreadExecutor;)V", shift = At.Shift.AFTER))
     private void onOnUnlockRecipes(UnlockRecipesS2CPacket packet, CallbackInfo ci) {
-        ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        assert player != null;
+        if (ConnectionInfo.protocolVersion <= Protocols.V1_16_1) {
+            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+            assert player != null;
 
-        // Copy over old options that weren't sent on-thread to avoid a race condition
-        RecipeBook oldRecipeBook = player.getRecipeBook();
-        RecipeBookOptions newOptions = packet.getOptions();
+            // Copy over old options that weren't sent on-thread to avoid a race condition
+            RecipeBook oldRecipeBook = player.getRecipeBook();
+            RecipeBookOptions newOptions = packet.getOptions();
 
-        newOptions.setGuiOpen(RecipeBookCategory.BLAST_FURNACE, oldRecipeBook.isGuiOpen(RecipeBookCategory.BLAST_FURNACE));
-        newOptions.setFilteringCraftable(RecipeBookCategory.BLAST_FURNACE, oldRecipeBook.isFilteringCraftable(RecipeBookCategory.BLAST_FURNACE));
-        newOptions.setGuiOpen(RecipeBookCategory.SMOKER, oldRecipeBook.isGuiOpen(RecipeBookCategory.SMOKER));
-        newOptions.setFilteringCraftable(RecipeBookCategory.SMOKER, oldRecipeBook.isFilteringCraftable(RecipeBookCategory.SMOKER));
+            newOptions.setGuiOpen(RecipeBookCategory.BLAST_FURNACE, oldRecipeBook.isGuiOpen(RecipeBookCategory.BLAST_FURNACE));
+            newOptions.setFilteringCraftable(RecipeBookCategory.BLAST_FURNACE, oldRecipeBook.isFilteringCraftable(RecipeBookCategory.BLAST_FURNACE));
+            newOptions.setGuiOpen(RecipeBookCategory.SMOKER, oldRecipeBook.isGuiOpen(RecipeBookCategory.SMOKER));
+            newOptions.setFilteringCraftable(RecipeBookCategory.SMOKER, oldRecipeBook.isFilteringCraftable(RecipeBookCategory.SMOKER));
+        }
     }
 }
