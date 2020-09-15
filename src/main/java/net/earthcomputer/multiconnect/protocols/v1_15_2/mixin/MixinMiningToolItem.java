0a1c4ea1bc66cbd606777eaf6104693307c0ee50
wagyourtail <ruwagray@gmail.com>
2020-09-15 01:40:51 -0700

    Fix hoe tool speed on 1.15.2 and below

---
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_15_2/mixin/MixinMiningToolItem.java
@@ -0,0 +1,24 @@
+package net.earthcomputer.multiconnect.protocols.v1_15_2.mixin;
+
+import net.earthcomputer.multiconnect.api.Protocols;
+import net.earthcomputer.multiconnect.impl.ConnectionInfo;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MiningToolItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+@Mixin(MiningToolItem.class)
+public class MixinMiningToolItem {
+
+    @Inject(at = @At("RETURN"), method="getMiningSpeedMultiplier", cancellable = true)
+    public void changeHoeEffectiveBlocks(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
+        if (ConnectionInfo.protocolVersion <= Protocols.V1_15_2 && (Object)this instanceof HoeItem) {
+            cir.setReturnValue(1.0F);
+        }
+    }
+}
