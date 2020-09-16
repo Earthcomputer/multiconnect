1329def61bfc4e0475663a2d084d246f1fd92d76
wagyourtail <ruwagray@gmail.com>
2020-09-15 18:05:33 -0700

    Update src/main/java/net/earthcomputer/multiconnect/protocols/v1_15_2/mixin/MixinMiningToolItem.java
    Co-authored-by: Joseph Burton <burtonjae@hotmail.co.uk>

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_15_2/mixin/MixinMiningToolItem.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_15_2/mixin/MixinMiningToolItem.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MiningToolItem.class)
 public class MixinMiningToolItem {
 
-    @Inject(at = @At("RETURN"), method="getMiningSpeedMultiplier", cancellable = true)
+    @Inject(method="getMiningSpeedMultiplier", at = @At("RETURN"), cancellable = true)
     public void changeHoeEffectiveBlocks(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
         if (ConnectionInfo.protocolVersion <= Protocols.V1_15_2 && (Object)this instanceof HoeItem) {
             cir.setReturnValue(1.0F);
