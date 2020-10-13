712c0d7e9dffadd8927d81be3aa3f7942917f1c6
Joe <burtonjae@hotmail.co.uk>
2020-10-13 20:39:54 +0100

    Fix Fabric registry sync issue in singleplayer

--- src/main/java/net/earthcomputer/multiconnect/mixin/compat/MixinRegistrySyncManager.java
+++ src/main/java/net/earthcomputer/multiconnect/mixin/compat/MixinRegistrySyncManager.java
@@ -2,33 +2,46 @@ package net.earthcomputer.multiconnect.mixin.compat;
 
 import com.google.common.base.Joiner;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.Packet;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Set;
+import java.util.function.Consumer;
 
 @Pseudo
 @Mixin(targets = "net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager", remap = false)
 public class MixinRegistrySyncManager {
+    @Unique
+    private static final Logger MULTICONNECT_LOGGER = LogManager.getLogger("multiconnect");
+
     @Dynamic
-    @ModifyVariable(method = "apply", at = @At(value = "STORE", ordinal = 0), ordinal = 1)
-    // The JVM doesn't validate this checked exception at the call site, seems to be implicitly allowed in the JLS.
-    // See https://stackoverflow.com/questions/12580598/what-parts-of-the-jls-justify-being-able-to-throw-checked-exceptions-as-if-they
-    private static CompoundTag onApplyModdedRegistry(CompoundTag mainTag) throws Exception {
+    @Inject(method = "createPacket", at = @At("HEAD"), cancellable = true)
+    private static void onCreatePacket(CallbackInfoReturnable<Packet<?>> ci) {
+        // don't try to sync other clients in LAN mode, or for whatever reason to the singleplayer owner
+        MULTICONNECT_LOGGER.info("Cancelling Fabric API registry sync packet server-side");
+        ci.setReturnValue(null);
+    }
+
+    @Dynamic
+    @Inject(method = "lambda$receivePacket$0", at = @At("HEAD"), cancellable = true)
+    private static void onApplyModdedRegistry(CompoundTag tag, Consumer<Exception> errorHandler, CallbackInfoReturnable<Object> ci) {
+        if (tag == null) {
+            return;
+        }
+        CompoundTag mainTag = tag.getCompound("registries");
         Set<String> registries = mainTag.getKeys();
         if (!registries.isEmpty()) {
             String registriesStr = Joiner.on(", ").join(registries);
-            try {
-                throw (Exception) Class.forName("net.fabricmc.fabric.impl.registry.sync.RemapException")
-                        .getConstructor(String.class)
-                        .newInstance("Server contains Fabric-modded registries: " + registriesStr + "! Multiconnect does not support modded registries.");
-            } catch (ReflectiveOperationException e) {
-                throw new RuntimeException(e);
-            }
+            errorHandler.accept(new RuntimeException("Server contains Fabric-modded registries: " + registriesStr + "! Multiconnect does not support modded registries."));
+            ci.setReturnValue(null);
         }
-        return mainTag;
     }
 }
