1758bc006fa0c64a670c95d5f5b55e99d5586f01
Joe <burtonjae@hotmail.co.uk>
2020-10-31 19:39:38 +0000

    Fix chunk unloading in 1.8

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_8/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_8/mixin/MixinClientPlayNetworkHandler.java
@@ -9,7 +9,9 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
 import net.minecraft.network.packet.s2c.play.EntityTrackerUpdateS2CPacket;
+import net.minecraft.network.packet.s2c.play.UnloadChunkS2CPacket;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.EulerAngle;
 import org.apache.logging.log4j.LogManager;
@@ -22,10 +24,20 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ClientPlayNetworkHandler.class, priority = -1000)
-public class MixinClientPlayNetworkHandler {
+public abstract class MixinClientPlayNetworkHandler {
     @Unique private static final Logger MULTICONNECT_LOGGER = LogManager.getLogger("multiconnect");
 
     @Shadow private ClientWorld world;
+
+    @Shadow public abstract void onUnloadChunk(UnloadChunkS2CPacket packet);
+
+    @Inject(method = "onChunkData", at = @At("HEAD"), cancellable = true)
+    private void onOnChunkData(ChunkDataS2CPacket packet, CallbackInfo ci) {
+        if (ConnectionInfo.protocolVersion <= Protocols.V1_8 && packet.isFullChunk() && packet.getVerticalStripBitmask() == 0) {
+            onUnloadChunk(new UnloadChunkS2CPacket(packet.getX(), packet.getZ()));
+            ci.cancel();
+        }
+    }
 
     @Inject(method = "onEntityTrackerUpdate",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/util/thread/ThreadExecutor;)V", shift = At.Shift.AFTER),
