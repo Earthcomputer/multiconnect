c09057708b155e38549de21c6a7db4707249e351
Joe <burtonjae@hotmail.co.uk>
2020-11-02 20:24:33 +0000

    1.16.4

--- src/api/java/net/earthcomputer/multiconnect/api/Protocols.java
+++ src/api/java/net/earthcomputer/multiconnect/api/Protocols.java
@@ -5,7 +5,7 @@ package net.earthcomputer.multiconnect.api;
  */
 public class Protocols {
 
-    public static final int V1_16_4 = 1073741827;
+    public static final int V1_16_4 = 754;
     public static final int V1_16_3 = 753;
     public static final int V1_16_2 = 751;
     public static final int V1_16_1 = 736;
