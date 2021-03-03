116247aa7a48855ff1fb39a8d4eea3afdf4a9843
Joe <burtonjae@hotmail.co.uk>
2021-03-03 18:16:57 +0000

    Cleanup packet accessor mixins, fix Java 9+

--- src/main/java/net/earthcomputer/multiconnect/mixin/bridge/MixinChunkDataS2C.java
+++ src/main/java/net/earthcomputer/multiconnect/mixin/bridge/MixinChunkDataS2C.java
@@ -15,10 +15,7 @@ import net.minecraft.util.EightWayDirection;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.dimension.DimensionType;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -92,5 +89,6 @@ public abstract class MixinChunkDataS2C implements IChunkDataS2CPacket {
 
     @Override
     @Accessor
+    @Mutable
     public abstract void setData(byte[] data);
 }
