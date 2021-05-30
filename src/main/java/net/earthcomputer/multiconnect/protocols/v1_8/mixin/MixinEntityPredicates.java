24282d2f816c2650267e44e9d3cfe4fbecda3295
Joe <burtonjae@hotmail.co.uk>
2021-05-30 22:57:27 +0100

    Remove mixin warning

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_8/mixin/MixinEntityPredicates.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_8/mixin/MixinEntityPredicates.java
@@ -11,8 +11,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EntityPredicates.class)
 public class MixinEntityPredicates {
+    @SuppressWarnings("target")
     @Dynamic
-    @Redirect(method = "method_5915", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isMainPlayer()Z"))
+    @Redirect(method = "method_5915(Lnet/minecraft/entity/Entity;Lnet/minecraft/scoreboard/AbstractTeam;Lnet/minecraft/scoreboard/AbstractTeam$CollisionRule;Lnet/minecraft/entity/Entity;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isMainPlayer()Z"))
     private static boolean makeMainPlayerUnpushable(PlayerEntity player) {
         if (ConnectionInfo.protocolVersion <= Protocols.V1_8) {
             return false;
