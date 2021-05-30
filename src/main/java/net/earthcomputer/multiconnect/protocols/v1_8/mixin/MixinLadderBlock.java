9cfeac089614e1b2d50ba5d432b127889c1393be
Joe <burtonjae@hotmail.co.uk>
2021-05-30 17:21:43 +0100

    Forgot protocol version check again

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_8/mixin/MixinLadderBlock.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_8/mixin/MixinLadderBlock.java
@@ -1,5 +1,7 @@
 package net.earthcomputer.multiconnect.protocols.v1_8.mixin;
 
+import net.earthcomputer.multiconnect.api.Protocols;
+import net.earthcomputer.multiconnect.impl.ConnectionInfo;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LadderBlock;
@@ -22,11 +24,13 @@ public class MixinLadderBlock {
     
     @Inject(method = "getOutlineShape", at = @At("HEAD"), cancellable = true)
     private void onGetOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> ci) {
-        switch (state.get(LadderBlock.FACING)) {
-            case NORTH -> ci.setReturnValue(NORTH_SHAPE_1_8);
-            case SOUTH -> ci.setReturnValue(SOUTH_SHAPE_1_8);
-            case WEST -> ci.setReturnValue(WEST_SHAPE_1_8);
-            default -> ci.setReturnValue(EAST_SHAPE_1_8);
+        if (ConnectionInfo.protocolVersion <= Protocols.V1_8) {
+            switch (state.get(LadderBlock.FACING)) {
+                case NORTH -> ci.setReturnValue(NORTH_SHAPE_1_8);
+                case SOUTH -> ci.setReturnValue(SOUTH_SHAPE_1_8);
+                case WEST -> ci.setReturnValue(WEST_SHAPE_1_8);
+                default -> ci.setReturnValue(EAST_SHAPE_1_8);
+            }
         }
     }
 }
