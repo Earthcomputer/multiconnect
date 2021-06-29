8785332a1c09b2fb10c7dfd1ae8145588a049f71
Joe <burtonjae@hotmail.co.uk>
2021-06-29 17:15:34 +0100

    Fix blocks being placed in the opposite direction

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_12_2/mixin/MixinItemPlacementContext.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_12_2/mixin/MixinItemPlacementContext.java
@@ -2,40 +2,39 @@ package net.earthcomputer.multiconnect.protocols.v1_12_2.mixin;
 
 import net.earthcomputer.multiconnect.api.Protocols;
 import net.earthcomputer.multiconnect.impl.ConnectionInfo;
-import net.minecraft.block.CommandBlock;
-import net.minecraft.block.DispenserBlock;
-import net.minecraft.block.ObserverBlock;
-import net.minecraft.block.PistonBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin({PistonBlock.class, ObserverBlock.class, CommandBlock.class, DispenserBlock.class})
-public class MixinRotatableBlock {
-    @Redirect(method = "getPlacementState", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemPlacementContext;getPlayerLookDirection()Lnet/minecraft/util/math/Direction;"))
-    private Direction get112LookDirection(ItemPlacementContext context) {
-        PlayerEntity player = context.getPlayer();
+@Mixin(ItemPlacementContext.class)
+public class MixinItemPlacementContext {
+    @Inject(method = "getPlayerLookDirection", at = @At("HEAD"), cancellable = true)
+    private void get112LookDirection(CallbackInfoReturnable<Direction> ci) {
+        ItemPlacementContext self = (ItemPlacementContext) (Object) this;
+
+        PlayerEntity player = self.getPlayer();
         if (ConnectionInfo.protocolVersion <= Protocols.V1_12_2 && player != null) {
-            BlockPos placementPos = context.getBlockPos();
+            BlockPos placementPos = self.getBlockPos();
             if (Math.abs(player.getX() - (placementPos.getX() + 0.5)) < 2 && Math.abs(player.getZ() - (placementPos.getZ() + 0.5)) < 2) {
                 double eyeY = player.getY() + player.getEyeHeight(player.getPose());
 
                 if (eyeY - placementPos.getY() > 2) {
-                    return Direction.UP;
+                    ci.setReturnValue(Direction.DOWN);
+                    return;
                 }
 
                 if (placementPos.getY() - eyeY > 0) {
-                    return Direction.DOWN;
+                    ci.setReturnValue(Direction.UP);
+                    return;
                 }
             }
 
-            return player.getHorizontalFacing().getOpposite();
-        } else {
-            return context.getPlayerLookDirection();
+            ci.setReturnValue(player.getHorizontalFacing());
         }
     }
 }
