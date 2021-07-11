17c6d27fb46cfe43411427bbfd148f98316961e9
Joe <burtonjae@hotmail.co.uk>
2021-07-11 18:48:32 +0100

    Add more info to game log if #178 occurs, and prompt user to upload their log if it happens

--- src/main/java/net/earthcomputer/multiconnect/impl/DebugUtils.java
+++ src/main/java/net/earthcomputer/multiconnect/impl/DebugUtils.java
@@ -5,16 +5,27 @@ import net.earthcomputer.multiconnect.protocols.generic.AbstractProtocol;
 import net.earthcomputer.multiconnect.protocols.generic.IIdList;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ConfirmScreen;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
+import net.minecraft.text.BaseText;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Util;
 import net.minecraft.util.registry.Registry;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
@@ -22,9 +33,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class DebugUtils {
+    private static final String MULTICONNECT_ISSUES_URL = "https://github.com/Earthcomputer/multiconnect/issues/%d";
+    private static int rareBugIdThatOccurred = 0;
+    private static long timeThatRareBugOccurred;
+
     public static void dumpBlockStates() {
         for (int id : ((IIdList) Block.STATE_IDS).multiconnect_ids()) {
             BlockState state = Block.STATE_IDS.get(id);
@@ -94,5 +108,54 @@ public class DebugUtils {
                 .sorted(Comparator.comparingInt(entry -> entry.getData().getId()))
                 .map(entry -> entry.getData().getId() + ": " + getTrackedDataName(entry.getData()) + " = " + entry.get())
                 .collect(Collectors.joining("\n"));
+    }
+
+    public static void reportRareBug(int bugId) {
+        rareBugIdThatOccurred = bugId;
+        timeThatRareBugOccurred = System.nanoTime();
+
+        MinecraftClient mc = MinecraftClient.getInstance();
+        if (!mc.isOnThread()) {
+            mc.send(() -> reportRareBug(bugId));
+            return;
+        }
+
+        String url = MULTICONNECT_ISSUES_URL.formatted(rareBugIdThatOccurred);
+        mc.inGameHud.getChatHud().addMessage(new TranslatableText("multiconnect.rareBug", new TranslatableText("multiconnect.rareBug.link")
+                .styled(style -> style.withUnderline(true)
+                        .withColor(Formatting.BLUE)
+                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText(url)))
+                        .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url))))
+            .formatted(Formatting.YELLOW));
+    }
+
+    public static boolean wasRareBugReportedRecently() {
+        return rareBugIdThatOccurred != 0 && (System.nanoTime() - timeThatRareBugOccurred) < 10_000_000_000L;
+    }
+
+    private static BaseText getRareBugText(int line) {
+        String url = MULTICONNECT_ISSUES_URL.formatted(rareBugIdThatOccurred);
+        return new TranslatableText("multiconnect.rareBug", new TranslatableText("multiconnect.rareBug.link")
+                .styled(style -> style.withUnderline(true)
+                        .withColor(Formatting.BLUE)
+                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText(url)))
+                        .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url))));
+    }
+
+    public static Screen createRareBugScreen(Screen parentScreen) {
+        URL url;
+        try {
+            url = new URL(MULTICONNECT_ISSUES_URL.formatted(rareBugIdThatOccurred));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        rareBugIdThatOccurred = 0;
+        return new ConfirmScreen(result -> {
+            if (result) {
+                Util.getOperatingSystem().open(url);
+            }
+            MinecraftClient.getInstance().openScreen(parentScreen);
+        }, parentScreen.getTitle(), new TranslatableText("multiconnect.rareBug.screen"));
     }
 }
--- src/main/java/net/earthcomputer/multiconnect/mixin/connect/MixinMinecraftClient.java
+++ src/main/java/net/earthcomputer/multiconnect/mixin/connect/MixinMinecraftClient.java
@@ -1,13 +1,16 @@
 package net.earthcomputer.multiconnect.mixin.connect;
 
 import net.earthcomputer.multiconnect.impl.ConnectionInfo;
+import net.earthcomputer.multiconnect.impl.DebugUtils;
 import net.earthcomputer.multiconnect.protocols.ProtocolRegistry;
 import net.minecraft.SharedConstants;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.DisconnectedScreen;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
@@ -21,5 +24,13 @@ public abstract class MixinMinecraftClient {
         ConnectionInfo.protocolVersion = SharedConstants.getGameVersion().getProtocolVersion();
         ConnectionInfo.protocol = ProtocolRegistry.get(ConnectionInfo.protocolVersion);
         ConnectionInfo.protocol.setup(false);
+    }
+
+    @ModifyVariable(method = "openScreen", at = @At("HEAD"), argsOnly = true)
+    private Screen modifyScreen(Screen screen) {
+        if (screen instanceof DisconnectedScreen && DebugUtils.wasRareBugReportedRecently()) {
+            return DebugUtils.createRareBugScreen(screen);
+        }
+        return screen;
     }
 }
