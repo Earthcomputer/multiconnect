e3383f0b3bdc1f81dc30c5ebc220b4d0c4abd5fc
Joe <burtonjae@hotmail.co.uk>
2021-11-26 21:13:52 +0000

    1.18-rc3

--- src/api/java/net/earthcomputer/multiconnect/api/Protocols.java
+++ src/api/java/net/earthcomputer/multiconnect/api/Protocols.java
@@ -6,7 +6,7 @@ package net.earthcomputer.multiconnect.api;
 public class Protocols {
 
     // 1.18.x
-    public static final int V1_18 = 1073741881;
+    public static final int V1_18 = 1073741883;
 
     // 1.17.x
     public static final int V1_17_1 = 756;
