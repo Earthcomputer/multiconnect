d31f124a1e01cba65846bbf8f83e14d12aa13117
JaaiDead <jaaiashwath.2019@gmail.com>
2022-03-19 14:50:14 +0530

    Some Dev Changes -Jaai

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_18_2/Protocol_1_18_2.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_18_2/Protocol_1_18_2.java
@@ -3,4 +3,5 @@ package net.earthcomputer.multiconnect.protocols.v1_18_2;
 import net.earthcomputer.multiconnect.protocols.generic.AbstractProtocol;
 
 public class Protocol_1_18_2 extends AbstractProtocol {
+
 }
