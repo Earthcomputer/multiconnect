3be8e4c879f277e530de3607a7f6a4c01ebf2de7
Joe <burtonjae@hotmail.co.uk>
2022-04-02 13:46:58 +0100

    Fix connection to 1.18.0

--- src/main/java/net/earthcomputer/multiconnect/protocols/v1_18/Protocol_1_18.java
+++ src/main/java/net/earthcomputer/multiconnect/protocols/v1_18/Protocol_1_18.java
@@ -1,17 +1,57 @@
 package net.earthcomputer.multiconnect.protocols.v1_18;
 
+import com.google.common.collect.Sets;
+import com.mojang.serialization.Codec;
 import net.earthcomputer.multiconnect.protocols.ProtocolRegistry;
 import net.earthcomputer.multiconnect.protocols.generic.TagRegistry;
 import net.earthcomputer.multiconnect.protocols.v1_18_2.Protocol_1_18_2;
+import net.earthcomputer.multiconnect.transformer.Codecked;
 import net.earthcomputer.multiconnect.transformer.VarInt;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.network.packet.s2c.play.EntityStatusEffectS2CPacket;
+import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import net.minecraft.network.packet.s2c.play.RemoveEntityStatusEffectS2CPacket;
 import net.minecraft.tag.BlockTags;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Unit;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.dimension.DimensionType;
 
 public class Protocol_1_18 extends Protocol_1_18_2 {
     public static void registerTranslators() {
+        ProtocolRegistry.registerInboundTranslator(GameJoinS2CPacket.class, buf -> {
+            buf.enablePassthroughMode();
+            buf.readInt(); // entity id
+            buf.readBoolean(); // hardcore
+            buf.readByte(); // game mode
+            buf.readByte(); // previous game mode
+            buf.readCollection(Sets::newHashSetWithExpectedSize, b -> RegistryKey.of(Registry.WORLD_KEY, b.readIdentifier())); // dimension ids
+            buf.disablePassthroughMode();
+            buf.decode(Codec.unit(() -> Unit.INSTANCE)); // registry manager
+            buf.decode(Codec.unit(() -> Unit.INSTANCE)); // dimension type
+            Identifier dimensionId = buf.readIdentifier();
+            DynamicRegistryManager.Mutable registryManager = DynamicRegistryManager.createAndLoad();
+            //noinspection unchecked
+            buf.pendingRead(
+                    (Class<Codecked<DynamicRegistryManager>>) (Class<?>) Codecked.class,
+                    new Codecked<>(DynamicRegistryManager.CODEC, registryManager)
+            );
+            //noinspection unchecked
+            buf.pendingRead(
+                    (Class<Codecked<RegistryEntry<DimensionType>>>) (Class<?>) Codecked.class,
+                    new Codecked<>(
+                            DimensionType.REGISTRY_CODEC,
+                            registryManager.get(Registry.DIMENSION_TYPE_KEY)
+                                    .getOrCreateEntry(RegistryKey.of(Registry.DIMENSION_TYPE_KEY, dimensionId))
+                    )
+            );
+            buf.pendingRead(Identifier.class, dimensionId);
+            buf.applyPendingReads();
+        });
         ProtocolRegistry.registerInboundTranslator(RemoveEntityStatusEffectS2CPacket.class, buf -> {
             buf.enablePassthroughMode();
             buf.readVarInt(); // entity id
