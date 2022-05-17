d395703a7209f50688b1f9a8f533b532c562c15c
Joe <burtonjae@hotmail.co.uk>
2022-05-17 18:24:15 +0100

    Fix batch file for empty args

--- tools.bat
+++ tools.bat
@@ -1,1 +1,7 @@
-@gradlew :tools:run -q --console=plain "--args=%*"
+@echo off
+IF "%1" == "" GOTO NO_ARGS
+gradlew :tools:run -q --console=plain "--args=%*"
+GOTO END
+:NO_ARGS
+gradlew :tools:run -q --console=plain
+:END
