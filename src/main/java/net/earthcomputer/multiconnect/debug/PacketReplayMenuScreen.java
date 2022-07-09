cd678d7c4f2761c8d2b737312ded348190056c89
RealIndrit <->
2022-07-09 18:49:30 +0200

    Fix conflict

--- src/main/java/net/earthcomputer/multiconnect/debug/PacketReplayMenuScreen.java
+++ src/main/java/net/earthcomputer/multiconnect/debug/PacketReplayMenuScreen.java
@@ -1,13 +1,12 @@
 package net.earthcomputer.multiconnect.debug;
 
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.screen.ScreenTexts;
-import net.minecraft.text.Text;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 
 public class PacketReplayMenuScreen extends Screen {
-    private static final Text TITLE = Text.translatable("multiconnect.debug.menu.title");
+    private static final Component TITLE = Component.translatable("multiconnect.debug.menu.title");
 
     public PacketReplayMenuScreen() {
         super(TITLE);
@@ -15,28 +14,28 @@ public class PacketReplayMenuScreen extends Screen {
 
     @Override
     protected void init() {
-        addDrawableChild(new ButtonWidget(width / 2 - 100, height / 2 + 20, 200, 20, Text.translatable("multiconnect.debug.menu.HttpServer.start"), button -> {
+        addRenderableWidget(new Button(width / 2 - 100, height / 2 + 20, 200, 20, Component.translatable("multiconnect.debug.menu.HttpServer.start"), button -> {
             PacketReplay.startHttpServer();
         }));
 
-        addDrawableChild(new ButtonWidget(width / 2 - 100, height / 2 + 20 + 30, 200, 20, Text.translatable("multiconnect.debug.menu.start"), button -> {
+        addRenderableWidget(new Button(width / 2 - 100, height / 2 + 20 + 30, 200, 20, Component.translatable("multiconnect.debug.menu.start"), button -> {
             PacketReplay.start();
         }));
 
-        addDrawableChild(new ButtonWidget(width / 2 - 100, height / 2 + 20 + 60, 200, 20, ScreenTexts.CANCEL, button -> {
-            close();
+        addRenderableWidget(new Button(width / 2 - 100, height / 2 + 20 + 60, 200, 20, CommonComponents.GUI_CANCEL, button -> {
+            onClose();
         }));
     }
 
     @Override
-    public void close() {
-        super.close();
+    public void onClose() {
+        super.onClose();
     }
 
     @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+    public void render(PoseStack matrices, int mouseX, int mouseY, float delta) {
         renderBackground(matrices);
-        drawCenteredText(matrices, textRenderer, TITLE, width / 2, height / 2 - 50, 0xffffff);
+        drawCenteredString(matrices, font, TITLE, width / 2, height / 2 - 50, 0xffffff);
         super.render(matrices, mouseX, mouseY, delta);
     }
 }
