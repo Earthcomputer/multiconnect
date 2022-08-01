bf95911b04d2ed5351be179ed3f2a09a76fc332c
Joe <burtonjae@hotmail.co.uk>
2022-08-01 23:21:28 +0100

    Fix type of chat preview query ID. Closes #450

--- src/main/java/net/earthcomputer/multiconnect/packets/SPacketChatPreview.java
+++ src/main/java/net/earthcomputer/multiconnect/packets/SPacketChatPreview.java
@@ -1,11 +1,14 @@
 package net.earthcomputer.multiconnect.packets;
 
 import net.earthcomputer.multiconnect.ap.MessageVariant;
+import net.earthcomputer.multiconnect.ap.Type;
+import net.earthcomputer.multiconnect.ap.Types;
 
 import java.util.Optional;
 
 @MessageVariant
 public class SPacketChatPreview {
+    @Type(Types.INT)
     public int queryId;
     public Optional<CommonTypes.Text> message;
 }
