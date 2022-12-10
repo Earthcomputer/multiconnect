b0f58a75558bd110c4a0d5559916fbb8476c43ac
LostLuma <lilly@lostluma.dev>
2022-12-10 20:28:08 +0100

    Fix fabric-api compatibilty

--- src/main/java/net/earthcomputer/multiconnect/mixin/connect/BuiltInRegistriesMixin.java
+++ src/main/java/net/earthcomputer/multiconnect/mixin/connect/BuiltInRegistriesMixin.java
@@ -3,13 +3,14 @@ package net.earthcomputer.multiconnect.mixin.connect;
 import net.earthcomputer.multiconnect.protocols.generic.MulticonnectAddedRegistryEntries;
 import net.minecraft.core.registries.BuiltInRegistries;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BuiltInRegistries.class)
 public class BuiltInRegistriesMixin {
-    @Inject(method = "bootStrap", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/registries/BuiltInRegistries;freeze()V"))
+    @Inject(method = "createContents()V", at = @At("HEAD"))
     private static void preFreezeRegistries(CallbackInfo ci) {
         MulticonnectAddedRegistryEntries.register();
     }
