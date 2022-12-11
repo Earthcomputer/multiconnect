b6aee9b0983c49d474766ff2219a80b1812622b5
LostLuma <lilly@lostluma.dev>
2022-12-11 11:08:49 +0100

    Add content after vanilla content has been added

--- src/main/java/net/earthcomputer/multiconnect/mixin/connect/BuiltInRegistriesMixin.java
+++ src/main/java/net/earthcomputer/multiconnect/mixin/connect/BuiltInRegistriesMixin.java
@@ -3,14 +3,13 @@ package net.earthcomputer.multiconnect.mixin.connect;
 import net.earthcomputer.multiconnect.protocols.generic.MulticonnectAddedRegistryEntries;
 import net.minecraft.core.registries.BuiltInRegistries;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BuiltInRegistries.class)
 public class BuiltInRegistriesMixin {
-    @Inject(method = "createContents()V", at = @At("HEAD"))
+    @Inject(method = "createContents()V", at = @At("RETURN"))
     private static void preFreezeRegistries(CallbackInfo ci) {
         MulticonnectAddedRegistryEntries.register();
     }
